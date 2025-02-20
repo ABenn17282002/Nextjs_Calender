2626851856be36503414e05602e94a35fe304875
ABenn17282002 <a.benn17282002@gmail.com>
2025-02-20 07:59:12 +0000

    AuthRole_GitHubAuth

--- components/SocialButton.tsx
+++ components/SocialButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { IoLogoGoogle } from "react-icons/io5";
+import { IoLogoGoogle, IoLogoGithub } from "react-icons/io5";
 
 export const GoogleButton = () => {
     const handleSignIn = async () => {
@@ -20,6 +20,27 @@ export const GoogleButton = () => {
       >
         <IoLogoGoogle />
         Sign In with Google
+      </button>
+    );
+  };
+
+  export const GithubButton = () => {
+    const handleSignIn = async () => {
+      try {
+        await signIn("github", { redirectTo: "/dashboard" });
+      } catch (error) {
+        console.error("Sign-in error:", error);
+      }
+    };
+  
+    return (
+      <button
+        type="button"
+        onClick={handleSignIn}
+        className="flex items-center justify-center gap-1 py-2.5 rounded-lg uppercase text-white font-medium text-sm bg-gray-500 w-full"
+      >
+        <IoLogoGithub />
+        Sign In with Github
       </button>
     );
   };
